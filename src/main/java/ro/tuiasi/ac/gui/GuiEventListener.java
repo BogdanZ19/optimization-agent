f8a75d43e5ab4ea29de481c15bac5d46f1bd185b
BogdanZ19 <bogdanzainia1@gmail.com>
2026-04-25 17:13:28 +0300

    make gui file structure

---
+++ src/main/java/ro/tuiasi/ac/gui/GuiEventListener.java
@@ -0,0 +1,14 @@
+package ro.tuiasi.ac.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuiEventListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
