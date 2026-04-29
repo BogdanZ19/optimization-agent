d95ace744607c8fa984bd0d8e9ec38ada47ef19d
RubeNN55 <gabrielgrigorut2@gmail.com>
2026-04-29 11:31:33 +0300

    implement CodeReaderService and unit tests

--- src/test/java/ro/tuiasi/ac/service/FolderScannerServiceTest.java
+++ src/test/java/ro/tuiasi/ac/service/FolderScannerServiceTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FolderScannerServiceTest {
+public class FolderScannerServiceTest {
 
     private FolderScannerService folderScannerService;
 
